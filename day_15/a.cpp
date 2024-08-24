1ce145536594cd75b4be58c176634eedccd13e08
yash-op7 <yashm3375@gmail.com>
2024-08-24 06:39:23 +0530

    RC, NIDCAA, LC past 10 contests speedrun, 20 questions - 2024-08-24 06:39:13

---
+++ day_15/a.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define vvvb vector<vvb>
+#define pq priority_queue
+#define ll long long
+#define all(x) (x).begin(), (x).end()
+#define vl vector<ll>
+#define vvl vector<vl>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define inp(n, a) for(int i=0;i<n;i++) cin >> a[i];
+#define pb push_back
+const ll INF = 1e12;
+const int MOD = 1e9 + 7;
+
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+    int tt;
+    cin >> tt;
+    while(tt--) solve();
+}
+
---
+++ day_16/log.md
@@ -0,0 +1,1 @@
+Solving the harder problems from the last 10 contests on Leetocodo - 2024-08-24 06:39:13
