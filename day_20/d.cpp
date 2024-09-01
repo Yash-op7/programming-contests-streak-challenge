835e864227c11c0e6b19881d7a159eb60b816a8c
yash-op7 <yashm3375@gmail.com>
2024-09-01 21:09:56 +0530

    solved d

--- day_20/d.cpp
+++ day_20/d.cpp
@@ -18,8 +18,42 @@ using namespace std;
 const ll INF = 1e12;
 const int MOD = 1e9 + 7;
 
+void dfs(int curr, auto&ans, auto&a, auto&str) {
+    int val = 0;
+    int next = a[curr-1];
+    if(str[curr-1] == '0') val++;
+    while(next != curr) {
+        if(str[next-1] == '0') val++;
+        next = a[next-1];
+    }
+
+    next = a[curr-1];
+    ans[curr] = val;
+    while(next != curr) {
+        ans[next] = val;
+        next = a[next-1];
+    }
+}
+
 void solve() {
+    int n;
+    cin >> n;
+    vi a(n, 0);
+    inp(n, a);
+    string str;
+    cin >> str;
+    vector<int> ans(n+1, -1);
+
+    for(int i=1;i<=n;i++) {
+        if(ans[i] == -1) {
+            dfs(i, ans, a, str);
+        }
+    }
     
+    for(int i=1;i<=n;i++) {
+        cout << ans[i] << ' ';
+    }
+    cout << '\n';
 }
 
 int main() {
