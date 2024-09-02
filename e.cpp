7fc5cdd6919b188feee36838f5960bb283491111
yash-op7 <yashm3375@gmail.com>
2024-09-02 15:19:07 +0530

    contest over

--- e.cpp
+++ e.cpp
@@ -24,8 +24,8 @@ void solve() {
     string str;
     cin >> str;
     int ans = INT_MAX;
-    unordered_map<char, int>m1, m2;
     if(n %2 == 0) {
+        unordered_map<char, int>m1, m2;
         for(int i=0;i<n;i+=2) {
             m1[str[i]]++;
             m2[str[i+1]]++;
@@ -39,7 +39,66 @@ void solve() {
         }
         ans = (n/2) - odd_mx + (n/2) - even_mx;
     } else {
+        vector<unordered_map<char, int>>m1(n), m2(n);
+        for(int i=0;i<n;i++) {
+            if(i>0) {
+                if(i%2 == 0) {
+                    m1[i] = m1[i-1];
+                    m1[i][str[i]]++;
+                } else {
+                    m2[i] = m2[i-1];
+                    m2[i][str[i]]++;
+                }
+            } else {
+                if(i%2 == 0) {
+                    m1[i][str[i]]++;
+                } else {
+                    m2[i][str[i]]++;
+                }
+            }
+        }
+        for(int i =0;i<n;i+=2) {
+            m1[i+1] = m1[i];
+        }
+        for(int i=2;i<n;i+=2) {
+            m2[i] = m2[i-1];
+        }
+        for(int i=0;i<n;i++) {
+            // left
+            int left1=0, left2=0;
+            unordered_map<char, int> temp1, temp2;
+            if(i>0) {
+                if(i %2 == 0) {
+                    if(i-2>=0) {
+                        temp1 = m1[i-2];
+                    }
+                    temp2 = m2[i-1];
+                } else {
+                    if(i-2>=0) {
+                        temp2 = m2[i-2];
+                    }
+                    temp1 = m1[i-1];
+                }
+            }
+            if(i%2 == 0) {
 
+            }
+            for(const x:m1[n-1]) {`
+                temp1[x.first] += x.second - m1[i][x.first];
+            }
+            for(const x:m2[n-1]) {
+                temp2[x.first] += x.second - m2[i][x.first];
+            }
+
+            int odd_mx=0, even_mx=0;
+            for(const auto x:temp1) {
+                odd_mx = max(odd_mx, x.second);
+            }
+            for(const auto x:temp2) {
+                even_mx = max(even_mx, x.second);
+            }
+            ans = (n/2) - odd_mx + (n/2) - even_mx + 1;
+        }
     }
     cout << ans << '\n';
 }
